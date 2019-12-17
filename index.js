75810a8be2fdbdfba9634019e7e3ff78acd18a4f
Zaya <zayazaw67@gmail.com>
2019-12-17 02:34:44 -0600

    reacts poop emoji to specific user

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { token } = require('./config.json');
+const user = require('./users.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log("Ready!")
 });
 
-client.on('message', (message, user) => {
-    console.log(message.content)
-    console.log(user)
+client.on('message', (message) => {
 
-    if (message.content !== '' && user == "186335926333472769") {
-        console.log(user)
-        console.log('yes')
+    if (message.content !== '' && user.id == "102471143180500992") {
         message.react("💩");
     };
 });
