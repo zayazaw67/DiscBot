6bfcc1a052deb3231756fcfd646d984159b9adee
Zaya <zayazaw67@gmail.com>
2019-12-17 01:27:21 -0600

    message sending to channel

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const Discord = require('discord.js');
-const { prefix, token } = require ('./config.json');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log("Ready!")
 });
 
-client.on('message', message => { 
-    console.log(message.content)
-})
+client.on('message', message => {
+    // console.log(message.content)
+
+    if (message.content.startsWith(`${prefix}kick`)) {
+        message.channel.send("working");
+    };
+});
 
 client.login(token);
